fa7c6556c07c59703e60bb80ef9b39bf7369a7bc
Derik Hammer <derik@sqlhammer.com>
2020-04-19 20:33:46 -0400

    Magic missle fires at dummy

--- App/Global/CombatVariables.gd
+++ App/Global/CombatVariables.gd
@@ -6,18 +6,9 @@ extends Node
 # var b = "text"
 onready var target_position : Vector2
 onready var source_position : Vector2
-onready var dummy_hp = 5
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
-func hit():
-	dummy_hp = dummy_hp - 1
-
-func is_dead():
-	if dummy_hp <= 0:
-		return true
-	else:
-		return false
